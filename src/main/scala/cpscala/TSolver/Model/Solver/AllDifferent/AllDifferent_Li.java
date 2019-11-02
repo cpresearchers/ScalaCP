d90a170a9e58ea8a008252abbb515a46f3187208
李哲 <leezear@live.cn>
2019-11-02 18:06:25 +0800

    Alldiff 改为继承

---
+++ src/main/scala/cpscala/TSolver/Model/Solver/AllDifferent/AllDifferent_Li.java
@@ -0,0 +1,222 @@
+package cpscala.TSolver.Model.Solver.AllDifferent;
+
+import cpscala.XModel.XVar;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Queue;
+
+public class AllDifferent_Li extends AllDifferent {
+    // 总
+    BitSet matchedMask;
+    BitSet unmatchedMask;
+    BitSet tmp;
+    BitSet[] A;
+    BitSet[] B;
+    BitSet[] C;
+    BitSet[] D;
+    BitSet t;
+    BitSet tt;
+    BitSet ts;
+    BitSet allowedEdges;
+    int arity;
+    int maxDomainSize;
+    int numBit;
+//    int[] gammaADense;
+//    int[] gammaASparse;
+//    int numGamma;
+//    int[] ADense;
+//    int[] ASparse;
+//    int numA;
+
+    ArrayList<Integer> ANodes = new ArrayList<>();
+    ArrayList<Integer> gamma = new ArrayList<>();
+
+    ArrayList<Integer> notANodes = new ArrayList<>();
+    ArrayList<Integer> notgamma = new ArrayList<>();
+
+    ArrayList<Integer> freeNodes = new ArrayList<>();
+
+    public AllDifferent_Li(ArrayList<XVar> XV) {
+        super(XV);
+        arity = vars.size();
+        maxDomainSize = values_to_id.size();
+        numBit = arity * maxDomainSize;
+        matchedMask = new BitSet(numBit);
+        unmatchedMask = new BitSet(numBit);
+        allowedEdges = new BitSet(numBit);
+        tmp = new BitSet(numBit);
+        A = new BitSet[vsize];
+        B = new BitSet[maxDomainSize];
+        C = new BitSet[maxDomainSize];
+        D = new BitSet[vsize];
+
+
+//        // 初始化两个稀疏集
+//        // 集合前半部分是非gamma的，集合后半部是gamma的
+//        gammaADense = new int[vsize];
+//        gammaASparse = new int[vsize];
+//
+//        numGamma = vsize - 1;
+//        // 初始化两个稀疏集
+//        // 集合前半部分是属于A，集合后半部不属于A
+//        ADense = new int[maxDomainSize];
+//        ASparse = new int[maxDomainSize];
+//        numA = maxDomainSize - 1;
+//
+//
+
+        for (int i = 0; i < vsize; ++i) {
+            A[i] = new BitSet(numBit);
+            D[i] = new BitSet(numBit);
+//
+//            gammaADense[i] = i;
+//            gammaASparse[i] = i;
+        }
+
+        for (int i = 0; i < maxDomainSize; ++i) {
+            B[i] = new BitSet(numBit);
+            C[i] = new BitSet(numBit);
+//
+//            ADense[i] = i;
+//            ASparse[i] = i;
+        }
+
+        t = new BitSet(numBit);
+        tt = new BitSet(numBit);
+        ts = new BitSet(numBit);
+    }
+
+
+    public boolean Solve() {
+        if (!preprocess())  //必然不可解，肯定不用解了，直接返回
+            return false;
+
+        ANodes.clear();
+        gamma.clear();
+        notANodes.clear();
+        notgamma.clear();
+        freeNodes.clear();
+
+        ArrayList<Edge> Max_M = Find_Max_Match();
+        ArrayList<Integer> freeNodes = new ArrayList<>();
+
+        for (int i = 0; i < arity; ++i) {
+            for (int j = 0; j < maxDomainSize; ++j) {
+                var idx = getIndex(i, j);
+                switch (bipartite[i][j]) {
+                    case 1:
+                        C[j].set(idx);
+                        D[i].set(idx);
+                        unmatchedMask.set(idx);
+                        break;
+                    case -1:
+                        A[i].set(idx);
+                        B[j].set(idx);
+                        matchedMask.set(idx);
+                        gamma.add(i);
+                        ANodes.add(j);
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        for (int i = 0; i < maxDomainSize; ++i) {
+            if (B[i].isEmpty()) {
+                freeNodes.add(i);
+            }
+        }
+
+//        System.out.println("---A---");
+//        for (var a : A) {
+//            System.out.println(a);
+//        }
+//        System.out.println("---B---");
+//        for (var a : B) {
+//            System.out.println(a);
+//        }
+//        System.out.println("---C---");
+//        for (var a : C) {
+//            System.out.println(a);
+//        }
+//        System.out.println("---D---");
+//        for (var a : D) {
+//            System.out.println(a);
+//        }
+
+        // step2
+        for (var v : freeNodes) {
+            allowedEdges.or(C[v]);
+        }
+
+        // step 3
+        //申请一些临时变量用来标记是否结束
+        boolean extended = false;
+        do {
+            for (int i = 0; i < vsize; ++i) {
+                tmp.clear();
+                tmp.or(allowedEdges);
+                tmp.and(D[i]);
+                if (!tmp.isEmpty()) {
+                    extended = true;
+                    allowedEdges.or(A[i]);
+                    gamma.add(i);
+                }
+            }
+
+            extended = false;
+            for (int i = 0; i < maxDomainSize; ++i) {
+                tmp.clear();
+                tmp.or(allowedEdges);
+                tmp.and(C[i]);
+                if (!tmp.isEmpty()) {
+                    extended = true;
+                    allowedEdges.or(B[i]);
+                    ANodes.add(i);
+                }
+            }
+        } while (extended);
+
+        // step 4
+
+        // step 5
+
+
+        return true;
+    }
+
+
+    // shengcheng gezhong shujujiegou
+    private void step1() {
+
+    }
+
+
+    private int getIndex(int a, int b) {
+        return a * maxDomainSize + b;
+    }
+
+//    private void swapGamma(int i, int j) {
+//        int tmp = gammaADense[i];
+//        gammaADense[i] = gammaADense[j];
+//        gammaADense[j] = tmp;
+//
+//        gammaASparse[gammaADense[i]] = i;
+//        gammaASparse[gammaADense[j]] = j;
+//    }
+//
+//    private void swapA(int i, int j) {
+//        int tmp = ADense[i];
+//        ADense[i] = ADense[j];
+//        ADense[j] = tmp;
+//
+//        ASparse[ADense[i]] = i;
+//        ASparse[ADense[j]] = j;
+//    }
+
+
+}
